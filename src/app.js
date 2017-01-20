cc7363e9af632e2065cf1c4f2bc9b1c1aa6b9047
David MacKintosh <dgawmackintosh@gmail.com>
2017-01-20 15:44:22 +0000

    Implements Jade templates and partials

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 'use strict';
 
 var express = require('express'),
-	posts = require('./mock/posts.json');
+	  posts = require('./mock/posts.json');
+
 var app = express();
 
-app.get('/', function(req, res) {
-	res.send("<h1>I am practising Express</h1>");
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+	res.render('index');
 });
 
-app.get('/blog/:title?', function(req, res) {
+app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
-	if(title === undefined) {
+	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction");
+		res.send("This page is under construction!");
 	} else {
-		var post = posts[title];
-		res.send(post);
-	};
+		var post = posts[title] || {};
+		res.render('post', { post: post});
+	}
 });
 
 app.listen(3000, function() {
-	console.log("The frontend server is running on port 3000");
+	console.log("The frontend server is running on port 3000!");
 });
